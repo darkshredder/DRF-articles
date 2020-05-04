8478561b035ad8be317b4595b0aa44acf8559e1b
Yash <srivastavayash58@gmail.com>
2020-05-04 15:15:01 +0530

    added get put and delete method for articles in normal django view forms

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path, include
-from .views import article_list
+from .views import article_list, article_detail
 urlpatterns = [
 
     path('', article_list ),
-]
+    path('<int:pk>/', article_detail),
+    ]
